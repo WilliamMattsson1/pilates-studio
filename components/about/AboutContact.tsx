ba90c427aa8cb4e810b75f00709007ac5310cdff
William Mattsson <william.mattsson@iths.se>
2025-11-25 17:44:24 +0100

    fix maps responsive bug

--- components/about/AboutContact.tsx
+++ components/about/AboutContact.tsx
@@ -25,15 +25,15 @@ const socials = [
 
 const AboutContact = () => {
     return (
-        <section className="px-6 md:px-20 pt-12 pb-8 bg-white flex flex-col gap-4">
+        <section className="px-6 md:px-20 pt-12 pb-2 bg-white flex flex-col">
             <TitleHeader
                 title="Get in Touch"
                 subtitle="Reach out to discuss classes or personal sessions"
                 alignment="left"
             />
 
-            <div className="flex flex-col lg:flex-row gap-6">
-                <div className="flex-1 flex flex-col gap-4 bg-secondary-bg p-5 rounded-lg shadow-lg">
+            <div className="flex flex-col lg:flex-row gap-10">
+                <div className="flex-1 flex flex-col gap-4 bg-secondary-bg p-5 rounded-lg shadow-lg min-h-[400px] lg:h-auto">
                     <h3 className="text-xl font-semibold text-gray-900">
                         Contact Details
                     </h3>
@@ -96,12 +96,11 @@ const AboutContact = () => {
                     </Link>
                 </div>
 
-                <div className="flex-1 rounded-lg overflow-hidden shadow-lg ">
+                {/* Google Map */}
+                <div className="flex-1 min-h-[400px] lg:h-auto rounded-lg overflow-hidden shadow-lg relative">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2003.4325953019447!2d17.636377478103572!3d59.85856282487493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465fcbf7464c29e5%3A0x88bf0461ad081078!2sStora%20Torget%2C%20Centrum%2C%20Uppsala!5e0!3m2!1ssv!2sse!4v1764087706102!5m2!1ssv!2sse"
-                        width="100%"
-                        height="100%"
-                        className="border-0"
+                        className="absolute top-0 left-0 w-full h-full border-0"
                         allowFullScreen={false}
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
