34b69140f462255cc2715db56c1181facbf2c283
William Mattsson <william.mattsson@iths.se>
2025-11-25 16:14:25 +0100

    enable smooth scroll to remove next.js warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" data-scroll-behavior="smooth">
             <body
                 className={`${roboto.variable} ${geistMono.variable} antialiased`}
             >
